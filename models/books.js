46a72609e98067e2ad27eada19a0700491950c9b
vincentweyh <vincent.weyh@livefr>
2016-12-05 16:36:42 +0100

    API for books

---
+++ models/books.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/apirest');
+
+var bookSchema = new mongoose.Schema({
+  title: String,
+  isbn: String,
+  price: String,
+  publisher: String,
+  author: String,
+});
+
+var bookModel = mongoose.model('books', bookSchema);
+
+// bookModel.create({title: "Harry potter", isbn: "9780747541462", price: "32", publisher: "Bloomsbury Publishing PLC", author: "J. K. Rowling"},
+//                   {title: "The Lord of the Rings", isbn: "0458907707", price: "28", publisher: "George Allen & Unwin", author: "J. R. R. Tolkien"},function(err, result) {
+//                     if(err){
+//                       console.log('BOOK: ' + err);
+//                     }
+//                     console.log('BOOK: ' + result);
+//                   });
+
+module.exports = bookModel;
---
+++ routes/books.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var Books = require('../models/books')
+
+/* GET all books. */
+router.get('/', function(req, res, next) {
+  Books.find(function (err, books){
+    if(err) return next(err);
+    res.json(books);
+  });
+});
+
+/* GET book by isbn. */
+router.get('/isbn/:isbn', function(req, res, next) {
+  Books.find({"isbn": req.params.isbn}, function (err, book){
+    if(err) return next(err);
+    res.json(book);
+  });
+});
+
+/* GET book by id. */
+router.get('/id/:id', function(req, res, next) {
+  Books.findById(req.params.id, function (err, book){
+    if(err) return next(err);
+    res.json(book);
+  });
+});
+
+/* POST book */
+router.post('/', function(req, res, next) {
+  Books.create(req.body, function (err, book) {
+    if (err) return next(err);
+    res.json(book);
+  });
+});
+
+/* PUT Update book by isbn */
+// router.put('/isbn/:isbn', function(req, res, next) {
+//   Books.find({"isbn": req.params.isbn}, function (err, book){
+//     if(err) return next(err);
+//     res.json(book);
+//   });
+// });
+
+/* PUT Update book by id */
+router.put('/id/:id', function(req, res, next) {
+  Books.findByIdAndUpdate(req.params.id, req.body, function (err, book){
+    if(err) return next(err);
+    res.json(book);
+  });
+});
+
+/* DELETE book by id */
+router.delete('/id/:id', function(req, res, next) {
+  Books.findByIdAndRemove(req.params.id, function (err, book){
+    if(err) return next(err);
+    res.json(book);
+  });
+});
+
+/* DELETE book by isbn */
+router.delete('/isbn/:isbn', function(req, res, next) {
+  Books.find({"isbn": req.params.isbn}, function (err, book){
+    if(err) return next(err);
+    res.json(book);
+  });
+});
+
+module.exports = router;
